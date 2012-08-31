27e11c0feef3e73a27548467a5758c5c1c0b6c99
jrburke <jrburke@gmail.com>
2012-08-31 15:18:24 -0700

    remove console.logs and put in html since some browsers do not have a working console when dev tools are closed.

--- www/js/app/main1.js
+++ www/js/app/main1.js
@@ -6,13 +6,15 @@ define(function (require) {
         backbone = require('backbone'),
         underscore = require('underscore');
 
-    console.log('backbone: ' + backbone.VERSION);
-    console.log('underscore: ' + underscore.VERSION);
-
     //A fabricated API to show interaction of
     //common and specific pieces.
     controller.setModel(model);
     $(function () {
         controller.render(lib.getBody());
+
+        //Display backbone and underscore versions
+        $('body')
+            .append('<div>backbone version: ' + backbone.VERSION + '</div>')
+            .append('<div>underscore version: ' + underscore.VERSION + '</div>');
     });
 });
